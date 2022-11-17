36eb56fae08ee36e7b774b4b92a44239e5511a14
Theologos Mavropoulos <rusinner88@gmail.com>
2022-11-17 18:02:53 +0200

    final

--- navbar/src/Navbar.js
+++ navbar/src/Navbar.js
@@ -6,6 +6,17 @@ import "./App.css";
 
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = "0px";
+    }
+  }, [showLinks]);
   return (
     <nav>
       <div className="nav-center">
@@ -19,8 +30,8 @@ const Navbar = () => {
           </button>
         </div>
 
-        <div className="links-container">
-          <ul className="links">
+        <div className="links-container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
             {links.map((link) => {
               const { id, url, text } = link;
               return (
